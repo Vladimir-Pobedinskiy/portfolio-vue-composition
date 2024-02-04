058bc31c34d1316ba0d526871c646af5f2590ab4
Vladimir-Pobedinskiy <VladimirPobedinskiy@yandex.ru>
2024-02-04 21:41:05 +0300

    scrollController правки

--- src/composables/scrollController.js
+++ src/composables/scrollController.js
@@ -4,17 +4,18 @@ export const scrollController = {
   disableScroll() {
     scrollController.scrollPosition = window.scrollY
     scrollController.paddingOffset = window.innerWidth - document.documentElement.clientWidth
-    document.body.classList.add('lock')
     document.body.style.cssText = `
+      position: fixed;
+      left: 0;
       top: -${scrollController.scrollPosition}px;
       padding-right: ${scrollController.paddingOffset}px;
+      width: 100%;
+      height: 100vh;
+      overflow: hidden;
     `
   },
   enableScroll() {
-    document.body.classList.remove('lock')
-    document.body.style.cssText = `
-      padding-right: '0px';
-    `
+    document.body.style.cssText = ''
     window.scroll({
       top: scrollController.scrollPosition
     })
